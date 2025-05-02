bebcd5ea0469bf1b6f47316ee78b3459802c7dfb
AnaCarinaPP <ancari1989@gmail.com>
2025-05-02 20:40:12 +0200

    add feature login

--- LogIn.java
+++ LogIn.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class LogIn {
     public static void main(String[] args) {
         //Escribe un programa que pida al usuario ingresar un nombre de usuario y una constraseña, si son iguales a los datos dados imprimirá en terminal: Acceso concedido, si no son iguales imprimirá en terminal: Nombre de usuario o contraseña incorrecta.
         //Averigua como hacer para poder ingresar el dato justo al lado de lo que termine la pregunta, es decir, que no haga un salto de línea.
 
+        Scanner form = new Scanner(System.in);
+        System.out.print("Ingresa tu nombre de usuario: ");
+        String nombreUs = form.nextLine();
+        System.out.print("Ingresa tu contraseña: ");
+        String password = form.nextLine();
+        form.close();
+        if (nombreUs.equals("acpp2510") && password.equals("p@ss1234")){
+            System.out.print("Acceso concedido");
+        } else {
+            System.out.print("Nombre de usuario o contraseña incorrecta");
+        }
     }
 }
