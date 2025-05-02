44c210a18d572afb2d3fba314ef960d649bffef8
AnaCarinaPP <ancari1989@gmail.com>
2025-05-02 20:09:38 +0200

    add feature weather

--- Weather.java
+++ Weather.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Weather {
     public static void main(String[] args) {
         //Escribe un programa que pida al usuario que ingrese la temperatura en celcius de su ciudad (La pregunta y la respuesta deberá estar en una sola línea) y según ello devuelva lo siguiente:
@@ -9,5 +10,22 @@ public class Weather {
         //Si es mayor a 36: Ahí no hay quien viva
 
         //Imprime el resultado
+        Scanner clima = new Scanner(System.in);
+        System.out.print("Hola... ¿Cuál es tu temperatura el día de hoy?");
+        double temperatura = clima.nextDouble();
+        clima.close();
+        if (temperatura<0) {
+            System.out.println("Te estás congelando");
+        } else if (temperatura <= 10.0){
+            System.out.println("Hace mucho frío");
+        } else if (temperatura <= 17){
+            System.out.println("Con un abrigo estás bien");
+        } else if (temperatura <= 25){
+            System.out.println("Parece que ha llegado el verano");
+        } else if (temperatura <= 35){
+            System.out.println("Que calooorrrrr");
+        } else if (temperatura>36){
+            System.out.println("Ahí no hay quien viva");
+        }
     }
 }
